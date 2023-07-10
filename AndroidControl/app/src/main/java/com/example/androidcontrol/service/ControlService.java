6f97b655817af5aa5c05cb1721e1ba377f743b76
mikanogami <mikanogami@gmail.com>
2023-07-10 09:49:57 -0700

    updated .gitignore

--- AndroidControl/app/src/main/java/com/example/androidcontrol/service/ControlService.java
+++ AndroidControl/app/src/main/java/com/example/androidcontrol/service/ControlService.java
@@ -1,18 +1,26 @@
 package com.example.androidcontrol.service;
 
+import static com.example.androidcontrol.utils.MyConstants.APP_SCREEN_PIXELS_WIDTH;
+import static com.example.androidcontrol.utils.MyConstants.FULL_SCREEN_PIXELS_HEIGHT;
+import static com.example.androidcontrol.utils.Utils.twoBytesToInt;
+
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.GestureDescription;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Path;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 import com.example.androidcontrol.utils.UtilsControl;
 
+import java.util.Arrays;
+
 public class ControlService extends AccessibilityService {
 
     private static final String TAG = "ControlService";
-
+    public static final float setSigFigs = 1000.f;
+    public static final int numBytesPerVal = 2;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
@@ -29,7 +37,7 @@ public class ControlService extends AccessibilityService {
         Log.d(TAG, "control service started");
 
         byte[] eventBytes = intent.getByteArrayExtra("event");
-        GestureDescription.StrokeDescription clickStroke = UtilsControl.bytesToStrokeDescription(eventBytes);
+        GestureDescription.StrokeDescription clickStroke = bytesToStrokeDescription(eventBytes);
 
         GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
         gestureBuilder.addStroke(clickStroke);
@@ -50,6 +58,30 @@ public class ControlService extends AccessibilityService {
 
         return Service.START_NOT_STICKY;
     }
+
+    public static GestureDescription.StrokeDescription bytesToStrokeDescription(byte[] bytes) {
+
+        long duration = twoBytesToInt(Arrays.copyOfRange(bytes, 4 * numBytesPerVal, 5 * numBytesPerVal));
+
+        float[] mappedCoords = new float[4];
+        float normVal;
+        for(int i = 0; i < 4; i++) {
+            normVal = twoBytesToInt(Arrays.copyOfRange(bytes, numBytesPerVal * i, numBytesPerVal * (i+1))) / setSigFigs;
+
+            // x-coords are at even indices (0 and 2), y-coords are at odd indices (1 and 3)
+            if(i % 2 == 0)  { mappedCoords[i] = APP_SCREEN_PIXELS_WIDTH * normVal; }
+            else            { mappedCoords[i] = FULL_SCREEN_PIXELS_HEIGHT * (1 - normVal); }
+        }
+
+        GestureDescription.StrokeDescription strokeDescription;
+        Log.d("bytesToStrokeDescription", mappedCoords[0] + " " + mappedCoords[1]);
+        Path clickPath = new Path();
+        clickPath.moveTo(mappedCoords[0], mappedCoords[1]);
+        clickPath.lineTo(mappedCoords[2], mappedCoords[3]);
+
+        return new GestureDescription.StrokeDescription(clickPath, 0, duration);
+    }
+
 
 }
 
