f1320015c247d4de4ec74d1257c217baf5db1831
mikanogami <mikanogami@gmail.com>
2023-08-28 14:50:25 -0700

    minimalistic control service: - each gesture is a line between start and end point - works better than previous "drag" impl.

--- app/src/main/java/com/example/androidcontrol/service/ControlService.java
+++ app/src/main/java/com/example/androidcontrol/service/ControlService.java
@@ -22,17 +22,7 @@ public class ControlService extends AccessibilityService {
     private static final String TAG = "ControlService";
     public static final float setSigFigs = 1000.f;
     public static final int numBytesPerVal = 2;
-    public static final int DX_THRESHOLD = 50;
-    public static final int DY_THRESHOLD = 50;
-    public static final float DURATION_THRESHOLD = 500;
-    float prevStrokeX;
-    float prevStrokeY;
-    boolean isContinued = false;
-    long maxStrokeDuration = GestureDescription.getMaxGestureDuration() / (GestureDescription.getMaxGestureDuration() + 1);
-    long currentStrokeDuration = 0;
-    GestureDescription.StrokeDescription currentGesture;
-    GestureDescription.Builder gestureBuilder;
-    GestureDescription gestureConstraints;
+
     @Override
     public void onCreate() {
         Log.d("onCreate",  "service created");
@@ -54,72 +44,27 @@ public class ControlService extends AccessibilityService {
         Log.d(TAG, "control service started");
 
         byte[] eventBytes = intent.getByteArrayExtra(EVENT_BYTES_TAG);
-        boolean willContinue = intent.getBooleanExtra(WILL_CONTINUE_TAG, false);
 
-        long duration = twoBytesToInt(Arrays.copyOfRange(eventBytes, 4 * numBytesPerVal, 5 * numBytesPerVal));
-        if (duration <= 0) {
-            duration = 1;
-        }
-        currentStrokeDuration = duration;
         float[] eventCoords = bytesToScreenCoords(eventBytes);
         float x1 = eventCoords[0];
         float y1 = eventCoords[1];
         float x2 = eventCoords[2];
         float y2 = eventCoords[3];
-        //float currentX = eventCoords[2];
-        //float currentY = eventCoords[3];
-        gestureBuilder = new GestureDescription.Builder();
-        addStrokeToGesture(x1, y1, x2, y2);
-        dispatchGesture();
-        /*
-        if (!isContinued) {
-            gestureBuilder = new GestureDescription.Builder();
-            isContinued = true;
-            prevStrokeX = currentX;
-            prevStrokeY = currentY;
-            if (!willContinue) {
-                addStrokeToGesture(eventCoords[0], eventCoords[1], currentX, currentY);
-                dispatchGesture();
-            } else {
-                addStrokeToGesture(prevStrokeX, prevStrokeY, currentX, currentY);
-            }
-        } else {
-            gestureConstraints = gestureBuilder.build();
-            if (gestureConstraints.getStrokeCount() >= gestureConstraints.getMaxStrokeCount() - 1) {
-                addStrokeToGesture(prevStrokeX, prevStrokeY, currentX, currentY);
-                dispatchGesture();
-            }
 
-            if (!willContinue) {
-                addStrokeToGesture(prevStrokeX, prevStrokeY, currentX, currentY);
-                dispatchGesture();
-            } else {
-                if (isEventOutsideBounds(currentX, currentY)) {
-                    addStrokeToGesture(prevStrokeX, prevStrokeY, currentX, currentY);
-                }
-            }
-        }
-
-
-         */
-        return Service.START_NOT_STICKY;
-    }
-
-    private void addStrokeToGesture(float prevX, float prevY, float x, float y) {
+        GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
         Path path = new Path();
-        path.moveTo(prevX, prevY);
-        path.lineTo(x, y);
-        prevStrokeX = x;
-        prevStrokeY = y;
-
+        path.moveTo(x1, y1);
+        path.lineTo(x2, y2);
         GestureDescription.StrokeDescription gesture = new GestureDescription.StrokeDescription(
                 path, 0, 1);
         gestureBuilder.addStroke(gesture);
-        currentStrokeDuration = 0;
+        dispatchGesture(gestureBuilder);
+
+        return Service.START_NOT_STICKY;
     }
 
-    private void dispatchGesture() {
-        boolean result = dispatchGesture(gestureBuilder.build(), new GestureResultCallback() {
+    private void dispatchGesture(GestureDescription.Builder builder) {
+        boolean result = dispatchGesture(builder.build(), new GestureResultCallback() {
             @Override
             public void onCompleted(GestureDescription gestureDescription) {
                 super.onCompleted(gestureDescription);
@@ -132,7 +77,6 @@ public class ControlService extends AccessibilityService {
             }
         }, null);
         Log.d(TAG, "Gesture dispatched, result=" + result);
-        isContinued = false;
     }
 
     public static float[] bytesToScreenCoords(byte[] bytes) {
@@ -146,10 +90,7 @@ public class ControlService extends AccessibilityService {
             if(i % 2 == 0)  { mappedCoords[i] = APP_SCREEN_PIXELS_WIDTH * normVal; }
             else            { mappedCoords[i] = FULL_SCREEN_PIXELS_HEIGHT * (1 - normVal); }
         }
-
         return mappedCoords;
     }
-
-
 }
 
